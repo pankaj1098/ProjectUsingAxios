a96779eb38301c17a24a971ff856e13da59e55ae
pankaj1098 <pankajyadav.ce@gmail.com>
2022-12-01 03:23:58 +0530

    DeleteAppoinments

--- BAP.js
+++ BAP.js
@@ -26,19 +26,38 @@ function showNewUserOnScreen(user){
     document.getElementById('name').value = '';
     document.getElementById('PhoneNumber').value = '';
     const parentNode= document.getElementById('users');
-    const childHTML = `<li id='${user.email}'> '${user.name}' - '${user.email}' - '${user.PhoneNumber}'</li>`;
+    const childHTML = `<li id='${user._id}'> '${user.name}' - '${user.email}' - '${user.PhoneNumber}' <button onclick=deleteUser('${user._id}')>Delete</button></li>`;
     parentNode.innerHTML = parentNode.innerHTML + childHTML;
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-    const data=axios.get("https://crudcrud.com/api/6d66a0ab48e044ad9c8bbec1fbc2979f/appoinmentData")
+    axios.get("https://crudcrud.com/api/6d66a0ab48e044ad9c8bbec1fbc2979f/appoinmentData")
     .then((response) => {
-    // console.log(response)
+    console.log(response)
     for(var i=0; i< response.data.length; i++){ 
         showNewUserOnScreen(response.data[i])
     }
     })
     .catch((error) => { console.log(error)
     })
-    console.log(data);
 })
+
+function deleteUser(userId){
+    axios.delete(`https://crudcrud.com/api/6d66a0ab48e044ad9c8bbec1fbc2979f/appoinmentData/${userId}`)
+    .then((response)=>{
+        removeUserFromScreen(userId);
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+    // localStorage.removeItem(email);
+    
+}
+
+function removeUserFromScreen(userId){
+    const parentNode = document.getElementById("users");
+    const childNodeToBeDeleted= document.getElementById(userId);
+   if(childNodeToBeDeleted){
+    parentNode.removeChild(childNodeToBeDeleted)
+   }
+}
